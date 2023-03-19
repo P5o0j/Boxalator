58e480b2f88def3a507b6da40f69fc9ce146585a
Gerard Press <killerabs@gmail.com>
2023-03-19 15:31:23 +0000

    more work on the box calculation

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 const baseBox = 2;
 const newBox = 0;
 
-function calculation(rooms, people) {
+function clickHandler() {
+  const rooms = Number(document.getElementById("rooms").value);
+  const people = Number(document.getElementById("people").value);
+  console.log(rooms);
+  console.log(people);
+
   if (rooms === 1 && people === 1) {
+    console.log(`You only need ${baseBox} boxes!`);
+    const elementMain = document.querySelector(".hidden");
+    elementMain.classList.add("visible");
     const elementOne = document.querySelector(".hidden2");
     elementOne.classList.add("visible");
   } else {
-    const newBox = baseBox * rooms + baseBox * people;
-    return newBox;
+    console.log("need to calculate how many boxes");
+    const newBox = baseBox * (rooms + people);
+    console.log(`You only need ${newBox} boxes!`);
   }
 
-  console.log(newBox);
-}
+  // console.log("Rooms: " + rooms);
+  // console.log("People: " + people);
 
-function clickHandler() {
-  const rooms = document.getElementById("rooms").value;
-  const people = document.getElementById("people").value;
-  console.log("Rooms: " + rooms);
-  console.log("People: " + people);
+  // const elementMain = document.querySelector(".hidden");
+  // elementMain.classList.add("visible");
 
-  const elementMain = document.querySelector(".hidden");
-  elementMain.classList.add("visible");
+  // console.log(newBox);
 
-  console.log(newBox);
-
-  calculation(rooms, people);
+  // calculation(rooms, people);
 }
